7307fd248a633ca4ac6851e9056c58b97d7b59bf
Cristian727 <cristian.gonzalez7@hotmail.com>
2023-11-30 16:50:01 +0100

    funcion dados

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function generarTexto(){
 
 }
 
+function roll(){
+    
+const data = document.querySelector("#input_dados").value
+console.log(data)
+const tirada = data.split("d")
+console.log(tirada)
+
+const mis_dados = []
+console.log(tirada[0])
+for (let i = 0; i < tirada[0]; i++){
+    const resultado = Math.floor(Math.random() * tirada[1] + 1)
+    mis_dados.push(resultado)
+}
+console.log(mis_dados)
+}
